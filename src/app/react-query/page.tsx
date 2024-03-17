e999b64d8d309f2073c6faba2acacd4ab9819733
rlagudals95 <ahovmf@naver.com>
2024-03-17 21:22:35 +0900

    feat: 서버컴포넌트 테스트

--- src/app/react-query/page.tsx
+++ src/app/react-query/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { TodoApp } from '@components/Suspense/TodoApp';
-import ReactQueryProvider from '../ReactQueryProvider';
+import ReactQueryProvider from './ReactQueryProvider';
 
 
 const ReactQueryHome: React.FC = () => {
